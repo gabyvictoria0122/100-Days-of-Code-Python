4d38e80920308e5da88ac6a77d82aaeed12ff513
Gabriela <gabriela.nascimento@buser.com.br>
2025-01-06 22:15:40 -0300

    fisrt day

---
+++ day_1.py
@@ -0,0 +1,8 @@
+
+print("Hey Guy, welcome to the  Band Name Generator!")
+print("Firt of all, answer someone questions:")
+
+name_city = input("Which city did you grow up in?\n")
+name_pet = input("What is your pet's name?\n")
+name_band = name_city + " " + name_pet
+print(f"Your band name is: {name_band}!")
